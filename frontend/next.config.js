7d41a722bcb9e9f620e2604c36e9df06871e76d8
nullnull <nullnullprogramming@gmail.com>
2024-02-12 19:09:59 +0900

    fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
+    images: {
+        domains: ['firebasestorage.googleapis.com'],
+      },
    
     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
     // trailingSlash: true,
