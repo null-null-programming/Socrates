4b6035bb71874b62291cc3151bd60868b781fbbd
nullnull <nullnullprogramming@gmail.com>
2024-02-12 21:03:07 +0900

    deploy-test

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,20 +1,22 @@
+const path = require('path');
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
-    images: {
-      domains: ['firebasestorage.googleapis.com'],
-    },
-   
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
-   
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
-   
-    // Optional: Change the output directory `out` -> `dist`
-    distDir: 'out',
-  }
-   
-  module.exports = nextConfig
+  images: {
+    domains: ['firebasestorage.googleapis.com'],
+  },
+
+  // Optional: Uncomment if needed
+  // trailingSlash: true,
+  // skipTrailingSlashRedirect: true,
+  // distDir: '.next',
+
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
+
+module.exports = nextConfig;
